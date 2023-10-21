7de14a6a1df5afbeb1b0868c7985bde40d5a1753
Max Kolle <maxkolle1@gmail.com>
2023-10-21 15:35:23 +0100

    Define and demonstrate basic panic!

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "C" fn _start() -> !{
     println!("Hello World, this is {}: a basic operating system for learning.", "NUGGET");
+    panic!("Oops! Something went terribly wrong. Please restart the machine.");
 
     loop {}
 }
 
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
