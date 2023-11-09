b36dc7da6cb6a29aa42d9b1bd7f47a6da5f92333
Max Kolle <maxkolle1@gmail.com>
2023-11-09 21:41:12 +0000

    Write success exit code on test runner conclusion

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    // Now that we've run all our tests, quit from QEMU
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -19,6 +21,22 @@ fn trivial_assertion() {
     print!("testing harness... ");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 mod vga_buffer;
