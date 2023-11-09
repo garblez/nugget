004697ce183cfb5f93cd65332bd35875f258f68d
Max Kolle <maxkolle1@gmail.com>
2023-11-09 22:00:59 +0000

    Use conditional compilation for test panic handler

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn trivial_assertion() {
     serial_print!("testing harness... ");
-    assert_eq!(1, 1);
+    assert_eq!(1, 0);
     serial_println!("[ok]");
 }
 
@@ -55,9 +55,18 @@ pub extern "C" fn _start() -> !{
     loop {}
 }
 
-
+#[cfg(not(test))] // Normal panic handler
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)] // Test mode panic handler
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
+    loop {} // Kept as compiler doesn't know we're causing a program exit
 }
