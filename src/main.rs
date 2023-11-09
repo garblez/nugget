0d9fcd06ac0a004c9b551e55aeefe7d8b41e7993
Max Kolle <maxkolle1@gmail.com>
2023-11-09 21:20:33 +0000

    Add start point for custom test frameworks

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 #![no_std]
 #![no_main]
 
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
