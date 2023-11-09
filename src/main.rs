79f895fdfe2ad557e447820da7a34736c924aefa
Max Kolle <maxkolle1@gmail.com>
2023-11-09 21:28:01 +0000

    Add test harness and trivial assertion

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] // Make sure our test harness' entry point is called test_main to avoid passing over testing
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
@@ -13,6 +14,13 @@ fn test_runner(tests: &[&dyn Fn()]) {
     }
 }
 
+#[test_case]
+fn trivial_assertion() {
+    print!("testing harness... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -20,7 +28,10 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "C" fn _start() -> !{
     println!("Hello World, this is {}: a basic operating system for learning.", "NUGGET");
-    panic!("Oops! Something went terribly wrong. Please restart the machine.");
+    // panic!("Oops! Something went terribly wrong. Please restart the machine.");
+
+    #[cfg(test)]
+    test_main(); // Run tests conditionally in testing contexts
 
     loop {}
 }
