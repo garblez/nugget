7bedb96b567101bfa7754a914870655670a74e86
Max Kolle <maxkolle1@gmail.com>
2023-11-11 12:45:06 +0000

    Terminate after integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(nugget::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use nugget::println;
 
 #[no_mangle] // each integration test is its own binary so we need to give it a start point (unmangled obviously)
 pub extern "C" fn _start() -> ! {
@@ -13,12 +14,13 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    nugget::test_panic_handler(info)
 }
 
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output"); // verify that println works after booting
 }
