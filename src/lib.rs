0e4678abf2b4e5e27acad4615b5573688efa2888
Max Kolle <maxkolle1@gmail.com>
2023-11-13 15:36:50 +0000

    Add start IDT handler/initialiser

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 #![no_std]
-
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"] // Make sure our test harness' entry point is called test_main to avoid passing over testing
-
+#![feature(abi_x86_interrupt)]
 
 
 pub mod vga_buffer;
 pub mod serial;
+pub mod interrupts;
 
 use core::panic::PanicInfo;
 
