bdf98d721ad50bfc7a9256775d1bfe847130fd9e
Max Kolle <maxkolle1@gmail.com>
2023-12-02 16:46:24 +0000

    Mock frame allocation and write to vga from 0

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ entry_point!(kernel_main); // Type-check the entry point for the signature expec
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use nugget::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::{
+        structures::paging::Page,
+        VirtAddr,
+    };
 
     println!("Hello World, this is {}: a basic operating system for learning.", "NUGGET");
     // panic!("Oops! Something went terribly wrong. Please restart the machine.");
@@ -23,25 +26,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
 
     // initialise a memory Mapper
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    // Map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // Write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
     
-    let addresses = [
-        // identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
-
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt); // Use the crate Mapper translation
-        println!("{:?} -> {:?}", virt, phys);
-    }
-
     #[cfg(test)]
     test_main(); // Run tests conditionally in testing contexts
 
--- src/memory.rs
+++ src/memory.rs
@@ -2,8 +2,14 @@ use x86_64::{
     structures::paging::{
         PageTable,
         OffsetPageTable,
+        PhysFrame,
+        Mapper,
+        Size4KiB,
+        FrameAllocator,
+        Page,
     },
     VirtAddr,
+    PhysAddr,
 };
 
 /// Return a mutable reference to the active level 4 page table.
@@ -34,4 +40,34 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
     OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+
+/// Creates an example mapping from the given page to frame `0xb8000` (VGA buffer)
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: UNSAFE - only for testing this out
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+
+    map_to_result.expect("map_to failed").flush();
+}
+
+
+/// A FrameAllocator that just returns `None`
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        None
+    }
 }
