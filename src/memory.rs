558e08b8933360e4942ebe1b6f43a298c5e2505e
Max Kolle <maxkolle1@gmail.com>
2023-12-02 16:04:45 +0000

    Add virt addr translation

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,7 @@
 use x86_64::{
     structures::paging::PageTable,
     VirtAddr,
+    PhysAddr,
 };
 
 /// Return a mutable reference to the active level 4 page table.
@@ -20,4 +21,57 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr // Unsafe!
+}
+
+
+/// Translates the given virtual address to the
+/// mapped physical address, `None` if no address map exists.
+/// 
+/// Unsafe as the caller must guarantee the complete physical
+/// memory is mapped to virtual memory at the passed
+/// `physical_memory_offset`.
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+
+/// Private inner translation called by `translate_addr`
+/// 
+/// This function is safe to limit the scope of `unsafe` since
+/// Rust treats the whole body of unsafe functions as an unsafe
+/// block. This function must only be reachable through
+/// `unsafe fn` from outside of this module!
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::{
+        structures::paging::page_table::FrameError,
+        registers::control::Cr3,
+    };
+
+    // Read active level 4 frame from Cr3
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indices = [
+        addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()
+    ];
+
+    let mut frame = level_4_table_frame;
+
+    // Traverse the multi-level page table
+    for &index in &table_indices {
+        // Convert the frame into a page table reference
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        // Read the page table and update `frame`
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages are not supported"),
+        };
+    }
+
+    // Calculate the physical address by adding the page offset
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
