5d92d41bc92a9e63a117e06f5e31851beb046dcf
Max Kolle <maxkolle1@gmail.com>
2023-12-02 16:09:39 +0000

    Check translate_addr works manually in main

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,8 @@ use nugget::println;
 entry_point!(kernel_main); // Type-check the entry point for the signature expected by the bootloader.
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use nugget::memory::active_level_4_table;
-    use x86_64::{
-        structures::paging::PageTable,
-        VirtAddr,
-    };
+    use nugget::memory::translate_addr;
+    use x86_64::VirtAddr;
 
     println!("Hello World, this is {}: a basic operating system for learning.", "NUGGET");
     // panic!("Oops! Something went terribly wrong. Please restart the machine.");
@@ -23,51 +20,22 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     nugget::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    
+    let addresses = [
+        // identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() { // Only print non-empties so to not fill up the screen
-            println!("L4 Entry {}: {:?}", i, entry);
-
-            // Get the physical address from the entry and convert it
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            // Print non-empties of level 3 page table
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-
-                    // Get the physical address from the entry and convert it
-                    let phys = entry.frame().unwrap().start_address();
-                    let virt = phys.as_u64() + boot_info.physical_memory_offset;
-                    let ptr = VirtAddr::new(virt).as_mut_ptr();
-                    let l2_table: &PageTable = unsafe { &*ptr };
-
-                    // Print non-empties of level 2 page table
-                    for (i, entry) in l2_table.iter().enumerate() {
-                        if !entry.is_unused() {
-                            println!("      L2 Entry {}: {:?}", i, entry);
-
-                            // Get the physical address from the entry and convert it
-                            let phys = entry.frame().unwrap().start_address();
-                            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-                            let ptr = VirtAddr::new(virt).as_mut_ptr();
-                            let l1_table: &PageTable = unsafe { &*ptr };
-
-                            // Print non-empties of level 1 page table
-                            for (i, entry) in l1_table.iter().enumerate() {
-                                if !entry.is_unused() {
-                                    println!("          L1 Entry {}: {:?}", i, entry);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)]
